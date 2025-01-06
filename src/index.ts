bdd7ad0e6be963bd0cbe11e9b1f7643f475d3b0c
HyunTaek Oh <oht222@naver.com>
2025-01-07 02:03:00 +0900

    chore: prettier 적용

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-import express, { Express, Request, Response } from "express";
-import { MikroORM } from "@mikro-orm/core";
-import config from "src/mikro-orm.config";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import { MikroORM } from '@mikro-orm/core';
+import config from 'src/mikro-orm.config';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const main = async () => {
-    const orm = await MikroORM.init(config);
-    await orm.getMigrator().up();
+  const orm = await MikroORM.init(config);
+  await orm.getMigrator().up();
 
-    const app: Express = express();
-    const port = process.env.PORT || 8000;
+  const app: Express = express();
+  const port = process.env.PORT || 8000;
 
-    app.use(express.json());
-    app.set('orm', orm);
+  app.use(express.json());
+  app.set('orm', orm);
 
-    app.get("/", (req: Request, res: Response) => {
-        res.send("Hello World!");
-    });
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+  });
 
-    app.listen(port, () => {
-        console.log(`[server]: Server is running at http://localhost:${port}`);
-    });
-}
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+};
 
 main().catch((err) => {
-    console.error(err);
-    process.exit(1);
+  console.error(err);
+  process.exit(1);
 });
